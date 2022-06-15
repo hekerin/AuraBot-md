347c2ff62460c70e305d0ff8629fb6a1ec8c740a
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-06-15 21:48:00 +0800

    Delete plugins directory

--- plugins/anime-loli.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('https://rest-beni.herokuapp.com/api/randomimage/loli'), 'Nih', wm, 'NEXT', '.loli', m)
-}
-handler.help = ['loli']
-handler.tags = ['anime']
-handler.command = /^(loli)$/i
-
-module.exports = handler
--- plugins/anime-neko.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('https://api.waifu.pics/sfw/neko'), 'Nih', wm, 'NEXT', '.neko', m)
-}
-handler.help = ['neko']
-handler.tags = ['anime']
-handler.command = /^(neko)$/i
-
-module.exports = handler
