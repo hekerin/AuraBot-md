759edc8413bb79a254a4efee665f933de7d8e72d
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-06-15 22:27:15 +0800

    AuraBot-md

---
+++ config.js
@@ -0,0 +1,79 @@
+let fs = require('fs')
+global.owner = JSON.parse(fs.readFileSync('./src/owner.json')) // Put your number to folder /src/owner.json
+global.mods = JSON.parse(fs.readFileSync('./src/moderator.json')) // Want some help?
+
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.me',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  neoxr: 'https://neoxr-api.herokuapp.com',
+  amel: 'https://melcanz.com',
+  hardianto: 'https://hardianto.xyz',
+  lol: 'https://api.lolhuman.xyz',
+  adicug: 'https://api.adiofficial.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://neoxr-api.herokuapp.com': 'yntkts',
+  'https://api.xteam.xyz': 'apikeylu',
+  'https://melcanz.com': 'apikeylu',
+  'https://api.lolhuman.xyz': 'apikeylu',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.me': 'apikeylu',
+  'https://pencarikode.xyz': 'apikeylu',
+  'https://hardianto.xyz': 'hardianto',
+  'https://leyscoders-api.herokuapp.com': 'apikeylu',
+  'https://api.adiofficial.xyz': 'apikey lu'
+}
+
+//============= Wait and Eror ================//
+global.wait = '「 ⏱️ 」Harap tunggu...'
+global.eror = '「❗」Server ERROR!'
+
+//========Url Template Buttons=========//
+global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
+global.urlnya = "https://www.instagram.com/mas_.giyuu"
+
+//============= callButtons =============//
+global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
+global.phn = '+62 813-9322-7036'
+
+//============= Watermark Bot and Sticker =============//
+global.wm = '© AuraBot'
+global.author = '© AuraBot'
+global.packname ='RFIunknown'
+global.namabot = 'AuraBot'
+
+//============= Games ================//
+global.benar = '_*Benar✅*_'
+global.salah = '_*Salah❌*_'
+global.dikit = "dikit lagi, semangat ya :')"
+
+global.multiplier = 100 // The higher, The harder levelup
+
+//============= Thumb And Image ================//
+global.pp = 'https://telegra.ph/file/39bbded9693c9338069fd.jpg'
+global.image = 'https://telegra.ph/file/39bbded9693c9338069fd.jpg'
+global.thumb = 'http://telegra.ph/file/29f2d451412ae151a2dfb.jpg'
+
+//=========== Requirements ==========//
+
+global.baileys = require('@adiwajshing/baileys')
+global.fs = require('fs')
+global.data = JSON.parse(fs.readFileSync('./data.json'))
+global.fetch = require('node-fetch')
+global.bochil = require('@bochilteam/scraper')
+
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
