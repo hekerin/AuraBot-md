539a2e63220077a1c877ffc60c03cfa81fff9579
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-06-16 16:05:51 +0800

    Rapiin

--- plugins/creator.js
+++ plugins/creator.js
@@ -4,6 +4,10 @@ let handler = async(m, { conn }) => {
 	let id = m.chat
 	ftroli = { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { itemCount: 9999999999999999999999999999999999999999999999999999999, status: 1, surface: 1, message: wm, orderTitle: wm, sellerJid: '0@s.whatsapp.net' } } }
 	fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': require('fs').readFileSync('./src/logo.jpg'), thumbnail: require('fs').readFileSync('./src/logo.jpg'),sendEphemeral: true}}}
+        global.kontak2 = [
+       ['6281393227036', 'Rifai', 'Developer Bot', 'rfiunknown62@gmail.com', true],
+       ['628892964090', 'Aura', 'Lover', 'Nothing', true],
+        ]
 	conn.p[id] = [
 	await conn.sendKontak(m.chat, kontak2, fkontak, { contexInfo: { forwardingScore: 99999, isForwarded: true } })
 	]
