9a3f161c77b0fca5af900fd7c159f3284eb4f640
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-06-16 15:03:16 +0800

    Delete plugins directory

--- plugins/anime-cosplay.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('https://rest-beni.herokuapp.com/api/randomimage/cosplay'), 'Nih', wm, 'NEXT', '.neko', m)
-}
-handler.help = ['neko']
-handler.tags = ['anime']
-handler.command = /^(neko)$/i
-
-module.exports = handler
--- plugins/anime-husbu.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('https://rest-beni.herokuapp.com/api/randomimage/husbu'), 'Nih', wm, 'NEXT', '.husbu', m)
-}
-handler.help = ['husbu']
-handler.tags = ['anime']
-handler.command = /^(husbu)$/i
-
-module.exports = handler
